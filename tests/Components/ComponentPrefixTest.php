6014fd96ab6faa1a1c0a42828be5e303be2b6894
Maurizio <maurizio.lepora@gmail.com>
2021-04-27 21:39:16 +0200

    test: add `prefix` test

---
+++ tests/Components/ComponentPrefixTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Components;
+
+class ComponentPrefixTest extends ComponentTestCase
+{
+    protected function getEnvironmentSetUp($app): void
+    {
+        parent::getEnvironmentSetUp($app);
+
+        $app['config']->set('cms-ui.prefix', 'ui');
+    }
+
+    /** @test */
+    public function it_may_set_a_prefix(): void
+    {
+        $template = <<<'HTML'
+            <x-ui-layout.html>
+                <body>
+                    <h1>Hello world!</h1>
+                </body>
+            </x-ui-layout.html>
+            HTML;
+
+        $this->assertComponentMatches($template);
+    }
+}
